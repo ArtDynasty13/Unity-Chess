a2c70039b79c1cf97b35cccc266e1e1e3a5ff905
ArtDynasty13 <arthurwitte18@gmail.com>
2024-11-15 17:13:25 -0700

    Add files
    -WASD implemented. will skip over columns. must fix

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -16,16 +16,21 @@ public static class Piece
     public const int White = 10;
     public const int Black = 20;
 
-    public static int PieceColor(int piece){
+    public static int PieceColor(int piece)
+    {
         return piece == OutOfBounds ? OutOfBounds : piece == Empty ? Empty : piece < Black ? White : Black;
     }
 
-    public static int PieceType(int piece){
+    public static int PieceType(int piece)
+    {
         return piece == OutOfBounds ? OutOfBounds : piece == Empty ? Empty : piece < Black ? piece - White : piece - Black;
     }
 
-    public static string PieceToFenChar(int piece){
-        switch(piece){
+    // Existing method to convert piece to FEN character
+    public static string PieceToFenChar(int piece)
+    {
+        switch (piece)
+        {
             case Pawn + White: return "P";
             case Pawn + Black: return "p";
             case Knight + White: return "N";
@@ -39,6 +44,21 @@ public static class Piece
             case King + White: return "K";
             case King + Black: return "k";
             default: return "";
+        }
+    }
+
+    // New method to convert piece type to string (like "Pawn", "Knight", etc.)
+    public static string PieceTypeToString(int piece)
+    {
+        switch (PieceType(piece))
+        {
+            case Pawn: return "Pawn";
+            case Knight: return "Knight";
+            case Bishop: return "Bishop";
+            case Rook: return "Rook";
+            case Queen: return "Queen";
+            case King: return "King";
+            default: return "Unknown";  // If piece type is undefined
         }
     }
 }
